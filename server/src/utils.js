389104a9d797f1d7bf85d3ad72a9e82d58eb0618
Dhananjoy Biswas <ebappa1971@gmail.com>
2019-04-15 00:13:57 +0600

    Add unrar

--- server/src/utils.js
+++ server/src/utils.js
@@ -1,13 +1,40 @@
 const split = require('split');
+const { Readable } = require('stream');
+const { createExtractorFromData } = require('node-unrar-js');
 
-const readLines = stream => new Promise((resolve, reject) => (
-  chunks => stream
-    .pipe(split(/\r?\n/))
-    .on('data', chunk => chunks.push(chunk.trim()))
-    .on('end', () => resolve(chunks))
-    .on('error', reject)
-)([]));
+const reduceStream = (stream, D, F) =>
+  new Promise((resolve, reject) =>
+    stream
+      .on('data', d => F(D, d))
+      .on('end', () => resolve(D))
+      .on('error', reject)
+  );
+
+const unrar = async stream => {
+  const chunks = await reduceStream(stream, [], (chunks, chunk) => chunks.push(chunk));
+  const { buffer } = Uint8Array.from(Buffer.concat(chunks));
+  const extractor = createExtractorFromData(buffer);
+  const [{ state, reason, msg }, data] = extractor.extractAll();
+  if (state === 'FAIL') {
+    throw new Error(`[${reason}] ${msg}`);
+  }
+  
+  const { files: [{ fileHeader: { name, }, extract: [ status, array ] }] } = data;
+  if (status.state === 'FAIL') {
+    throw new Error(`[${status.reason}] ${status.msg}`);
+  }
+
+  const content = new Readable();
+  content.push((array));
+  content.push(null);
+
+  return { name, content };
+};
+
+const readLines = stream =>
+  reduceStream(stream.pipe(split(/\r?\n/)), [], (chunks, chunk) => chunks.push(chunk.trim()));
 
 module.exports = {
-  readLines
+  readLines,
+  unrar
 };
