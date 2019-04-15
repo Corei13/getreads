866bbbd4c55cd7253298738d37aab2a8bdddb74b
nowshad <iamnowshad@gmail.com>
2019-04-15 10:20:40 +0600

    Improve the extension.

--- extension/foreground.js
+++ extension/foreground.js
@@ -6,5 +6,14 @@ let btn = document.createElement("button");
 btn.innerText = "Download From GetReads";
 btn.className = "buttonBar";
 
+btn.addEventListener("click", function() {
+  this.disabled = true;
+  this.innerText = "Downloading your book ...";
+  setTimeout(() => {
+    btn.innerText = "Download From GetReads";
+    this.disabled = false;
+  }, 3000);
+});
+
 li.appendChild(btn);
 el[0].appendChild(li);
