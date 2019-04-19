f40a8300e1bc8a085a2263283493705a42b2834f
nowshad <iamnowshad@gmail.com>
2019-04-19 17:35:29 +0600

    Update frontend

--- extension/foreground.js
+++ extension/foreground.js
@@ -1,5 +1,34 @@
 console.log("Good reads!!");
 
+const isbn = document
+  .querySelector('meta[property="books:isbn"]')
+  .getAttribute("content");
+const author = document
+  .querySelector('meta[property="books:author"]')
+  .getAttribute("content")
+  .split("/")
+  .pop()
+  .split(".")
+  .pop()
+  .replace("_", " ");
+const title = document
+  .querySelector('meta[property="og:title"]')
+  .getAttribute("content");
+const id = document
+  .querySelector('meta[property="al:ios:url"]')
+  .getAttribute("content")
+  .split("/")
+  .pop();
+
+console.log({ isbn, id, author, title });
+
+(async () => {
+  const search = await fetch(
+    `https://rare-kangaroo-30.localtunnel.me/search?title=${title}&&author=${author}`
+  ).then(async res => await res.json());
+  console.log(search);
+})();
+
 let el = document.getElementsByClassName("buyButtonBar");
 let li = document.createElement("li");
 let btn = document.createElement("button");
@@ -19,3 +48,9 @@ li.appendChild(btn);
 el[0].appendChild(li);
 
 // TODO: add buttons in search page
+
+// document.querySelector('meta[property="books:isbn"]').getAttribute('content')
+// books:author
+// og:title
+// al:ios:url
+// books:isbn
--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,23 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const IRC = require('./irc');
+const IRC = require("./irc");
 
 const app = express();
-app.use(bodyParser.json({ limit: '200mb' }));
+app.use(bodyParser.json({ limit: "200mb" }));
 
 app.use((req, res, next) => {
   req.requestTime = Date.now();
   next();
 });
 
-app.get('/search', async (req, res, next) => {
+app.get("/search", async (req, res, next) => {
   // TODO: get isbn and other metadata and story
-  const { query: { title, author } } = req;
+  const {
+    query: { title, author }
+  } = req;
   try {
     const result = await search(title, author);
     res.status(200).send(result);
@@ -25,15 +27,17 @@ app.get('/search', async (req, res, next) => {
   }
 });
 
-app.get('/download', async (req, res, next) => {
+app.get("/download", async (req, res, next) => {
   // TODO: get isbn and other metadata and story
-  const { query: { path } } = req;
+  const {
+    query: { path }
+  } = req;
   try {
     const { stream, size, filename, mime } = await IRC.download(path);
 
-    res.set('Content-disposition', 'attachment; filename=' + filename);
-    res.set('Content-Type', mime || 'application/octet-stream');
-    res.set('Content-Length', size);
+    res.set("Content-disposition", "attachment; filename=" + filename);
+    res.set("Content-Type", mime || "application/octet-stream");
+    res.set("Content-Length", size);
     stream.pipe(res);
     next();
   } catch (err) {
@@ -52,19 +56,37 @@ app.use(({ requestTime, method, originalUrl }) => {
   console.log(method, originalUrl, elapsed.toFixed(2));
 });
 
-const data = fs.readFileSync(path.resolve(__dirname, './data/Horla.list'))
+const data = fs
+  .readFileSync(path.resolve(__dirname, "./data/Horla.list"))
   .toString()
   .split(/\r?\n/)
-  .filter(l =>
-    l.startsWith('!Horla') &&
-    ['epub', 'mobi', 'pdf', 'awz3'].find(e => l.endsWith(e))
+  .filter(
+    l =>
+      l.startsWith("!Horla") &&
+      ["epub", "mobi", "pdf", "awz3"].find(e => l.endsWith(e))
   );
-const score = (l, t, a) => 2 * Number(l.includes(t)) + Number(l.includes(a));
-const search = (title, author) => data.filter(l => l.includes(title)).sort((a, b) => score(b, title, author) - score(a, title, author));
+const score = (l, t, a) =>
+  2 *
+    Number(
+      l
+        .trim()
+        .toLowerCase()
+        .includes(t.trim().toLowerCase())
+    ) +
+  Number(
+    l
+      .trim()
+      .toLowerCase()
+      .includes(a.trim().toLowerCase())
+  );
+const search = (title, author) =>
+  data
+    .filter(l => l.includes(title))
+    .sort((a, b) => score(b, title, author) - score(a, title, author));
 
 (async () => {
   await IRC.initialize();
   app.listen(4017, () => {
-    console.log('controller listening on port 4017!');
+    console.log("controller listening on port 4017!");
   });
 })().then(console.log, console.error);
